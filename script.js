08e8783f070e8680652459537eeb53061ca37206
JARMARV <wagner.jarno@web.de>
2026-06-24 21:42:22 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const loginButton = document.getElementById("loginButton<")
+const loginPanel = document.getElementById("loginPanel")
+const adminCalenderSettingsPanel = document.getElementById("adminCalenderSettingsPanel")
+
+
+loginButton.addEventListener("click", event => {
+    loginPanel.style.display = "none";
+
+})
