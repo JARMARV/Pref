bde473f5a84e9d7cd9177a26a2bc0738de8c7e45
Jaronskkkkk <125912907+jaron232134@users.noreply.github.com>
2026-06-25 11:08:57 +0200

    grobes gerüst verbessert(admin acess)

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const loginButton = document.getElementById("loginButton<")
 const loginPanel = document.getElementById("loginPanel")
 const adminCalenderSettingsPanel = document.getElementById("adminCalenderSettingsPanel")
-
+const darkenedSite =document.getElementById("darkenedSite")
+const newTableButton =document.getElementById("newTableButton")
+const submitCalenderSettingsButton = document.getElementById("submitCalenderSettingsButton")
 
 loginButton.addEventListener("click", event => {
     loginPanel.style.display = "none";
-
+    darkenedSite.style.display = "none";
+})
+newTableButton.addEventListener("click", event => {
+    adminCalenderSettingsPanel.style.display = "grid";
+    darkenedSite.style.display = "block";
+    
+})
+submitCalenderSettingsButton.addEventListener("click", event => {
+    adminCalenderSettingsPanel.style.display ="none";
+    darkenedSite.style.display = "none";
 })
