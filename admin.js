9d41476190fbdc9aaab95f5c90370c200e5c0afb
JARMARV <wagner.jarno@web.de>
2026-07-12 19:36:12 +0200

    modules working and horizontal line sizing
    The Modules can now be manually set in the json file while js calculates where to render the modules. also made the horizontal hour mark lines scale dynamically

--- admin.js
+++ admin.js
@@ -15,6 +15,7 @@ const startTime = document.getElementById("startTime")
 const endTime = document.getElementById("endTime")
 const startDate = document.getElementById("startDate")
 const endDate = document.getElementById("endDate")
+const hourIncrement = 90
 
 //data for the module creation
 const dayOfModule = document.getElementById("dayOfModule")
@@ -30,13 +31,23 @@ var userAuthrization = "localAdmin"
 
 const monthsOfTheYear = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 const daysOfTheWeek = ["MO", "TU", "WE", "TH", "FR", "SA", "SU"]
-const weekOffsetIndex = 0
+const currentWeekIndex = 0
 
 
 let userData =[];
 fetch("userData.json")
     .then(response => response.json())
     .then(data => userData = data)
+
+
+// horizontal lines dynamic sizing  
+function updateCalendarColumnsWidth() {
+    const width = calendarColumns.offsetWidth;
+    document.documentElement.style.setProperty('--calendarColumnsWidth', width + 'px');
+}
+window.addEventListener('load', updateCalendarColumnsWidth);
+window.addEventListener('resize', updateCalendarColumnsWidth);
+
 
 newTableButton.addEventListener("click", event => {
     adminCalendarSettingsPanel.style.display = "grid";
@@ -46,6 +57,8 @@ newTableButton.addEventListener("click", event => {
     startTime.value = "05:30";
     endTime.value = "10:20";
 })
+
+
 submitCalendarSettingsButton.addEventListener("click", event => {
     if (!endDate.value || !startDate.value || !startTime.value || !endTime.value){
         alert("please fill out all information")
@@ -57,6 +70,7 @@ submitCalendarSettingsButton.addEventListener("click", event => {
     }
 })
 
+
 function drawCalendar(){
 
 }
@@ -64,14 +78,15 @@ function drawCalendar(){
 function updateCalendar(){
     drawCalendarTimeColumn()
     drawDateRow()
+    drawModules()
 }
 
 function drawCalendarTimeColumn(){
     calendarTimeColumn.innerHTML = ``;
-    const startTimeValue = startTime.value.split(":");
-    const endTimeValue = endTime.value.split(":");
+    const startTimeValue = userData[0].events[0].startDate.split("T")[1].split(":");
+    const endTimeValue = userData[0].events[0].endDate.split("T")[1].split(":");
     const maxTimeSpan = (parseInt(endTimeValue[0])+ 1) - (parseInt(startTimeValue[0])-1);
-    mainCalendar.style.height = `${(maxTimeSpan * 90)+100}px`;
+    mainCalendar.style.height = `${(maxTimeSpan * hourIncrement)+100}px`;
     for (let i = 0; i < maxTimeSpan; i++){
         calendarTimeColumn.innerHTML += `
         <div class="calendarTimeSlot">${parseInt(startTimeValue[0]) + i}:00 </div>
@@ -82,17 +97,27 @@ function drawCalendarTimeColumn(){
 
 function drawModules(){
 
+    //get the starting hour of the event
+    //alert(new Date(userData[0].events[0].startDate).getHours())
+    //get the time range of the event
+    //alert(((new Date(userData[0].events[0].endDate).getTime()-new Date(userData[0].events[0].startDate).getTime())/3600000)%24)
+    for (let i = 0; i < userData[0].events[0].modules.length; i++){
+        if (userData[0].events[0].modules[i].moduleWeekIndex === currentWeekIndex ){
+            const ModuleStartDate = new Date(userData[0].events[0].modules[i].start)
+            calendarColumns.children[ModuleStartDate.getDay()-1].innerHTML +=`
+                <div class="CalendarModule" style="top:${((ModuleStartDate.getHours()+0.5+(ModuleStartDate.getMinutes()/60))-new Date(userData[0].events[0].startDate).getHours())*hourIncrement}px ; height:${((new Date(userData[0].events[0].modules[i].end).getTime()-new Date(userData[0].events[0].modules[i].start).getTime())/3600000)%24*hourIncrement}px"> </div>     
+             `
+        }       
+    }
 }
 
 function drawDateRow(){
     const StartDate = new Date(userData[0].events[0].startDate);
     const EndDate = new Date(userData[0].events[0].endDate);
-    
     for (let i = 0; i < (calendarDateRow.children.length); i++){
 
         const outputDate = new Date(StartDate);
-        outputDate.setDate(StartDate.getDate() + (i - StartDate.getDay() + 1) + weekOffsetIndex * 7);
-
+        outputDate.setDate(StartDate.getDate() + (i - StartDate.getDay() + 1) + currentWeekIndex* 7);
         calendarDateRow.children[i].innerHTML = `
             <div class="calendarDayName">${daysOfTheWeek[i]}</div>
             <div class="calendarDayNumber">${outputDate.getDate()}</div>
