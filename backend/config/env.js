7759f5516feb46e746f522d921156652332f5a0e
JARMARV <wagner.jarno@web.de>
2026-09-02 08:47:34 +0200

    Checking if environment Variables are defined

--- backend/config/env.js
+++ backend/config/env.js
@@ -14,3 +14,24 @@ export const {
     JWT_EXPIRES_IN,
     ORIGIN,
 } = process.env;
+
+const requiredVariables = [
+    "PORT",
+    "NODE_ENV",
+    "PGHOST",
+    "PGPORT",
+    "PGUSER",
+    "PGPASSWORD",
+    "PGDATABASE",
+    "JWT_SECRET",
+    "JWT_EXPIRES_IN",
+    "ORIGIN",
+];
+
+for (const key of requiredVariables) {
+    if (!process.env[key]) {
+        console.warn(
+            `Warning: ${key} is not defined in the environment variables.`
+        );
+    }
+}
